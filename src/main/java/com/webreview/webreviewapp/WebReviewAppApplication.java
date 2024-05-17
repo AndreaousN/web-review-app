656ac9df48dfd6a362380ed371ab92e8bcb1a1db
AndreaousN <a.nizevicius3@gmail.com>
2024-05-17 19:49:12 +0300

    initial commit

---
+++ src/main/java/com/webreview/webreviewapp/WebReviewAppApplication.java
@@ -0,0 +1,13 @@
+package com.webreview.webreviewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebReviewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebReviewAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webreview/webreviewapp/WebReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webreview.webreviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebReviewAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
